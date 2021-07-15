77eb4044f434eb65fdd34951b72902bab89470e2
Daniel Couzens <danecouzens@gmail.com>
2021-07-15 12:44:13 +0100

    header responsive

---
+++ src/components/NavButton.js
@@ -0,0 +1,11 @@
+// import React, { useState } from "react";
+
+// const NavButton = () => {
+//   const [navState, setNavState] = useState(false);
+//   const navStyles = navState ? "opened" : "";
+//   return (
+
+//   );
+// };
+
+// export default NavButton;
