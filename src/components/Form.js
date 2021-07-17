868aa2aed5bd9c6bbc2aa05cfc6f3a584ec1a6c2
Daniel Couzens <danecouzens@gmail.com>
2021-07-17 13:50:45 +0100

    form new

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,19 @@
 import React from "react";
 import { navigate } from "gatsby-link";
-import { Formik, Form } from "formik";
-import TextField from "../components/TextField";
-import MessageField from "../components/MessageField";
-import * as Yup from "yup";
+// import Layout from '../layout'
 
-const ContactForm = () => {
-  const validate = Yup.object({
-    name: Yup.string()
-      .max(30, "Must be 30 characters or less")
-      .required("Your name is required"),
-    email: Yup.string()
-      .email("Email is invalid")
-      .required("Your email is required"),
-    company: Yup.string()
-      .max(30, "Must be 30 characters or less")
-      .required("Your company name is required"),
-    telephone: Yup.string()
-      .max(30, "Must be 30 characters or less")
-      .required("Your Telephone number is required"),
-    message: Yup.string()
-      .max(250, "Must be 250 characters or less")
-      .required("Your message is required"),
-  });
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-  // const [state, setState] = React.useState({})
+export default function Contact() {
+  const [state, setState] = React.useState({});
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,7 +23,7 @@ const ContactForm = () => {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": form.getAttribute("name"),
-        // ...state,
+        ...state,
       }),
     })
       .then(() => navigate(form.getAttribute("action")))
@@ -42,42 +31,179 @@ const ContactForm = () => {
   };
 
   return (
-    <Formik
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        email: "",
-        message: "",
-      }}
-      validationSchema={validate}
-      onSubmit>
-      {(formik) => (
-        <div className="form">
-          <Form
-            name="contact"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            onSubmit={handleSubmit}>
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="contact-details">
-              <TextField label="Name" name="name" type="text" />
-              <TextField label="Email" name="email" type="email" />
-              <TextField label="Company" name="company" type="text" />
-              <TextField label="Telephone" name="telephone" type="text" />
-            </div>
-            <div className="contact-message">
-              <MessageField label="Message" name="message" type="textarea" />
-            </div>
-            <div className="button-wrap">
-              <button type="submit">Submit</button>
-              {/* <button type="reset">Reset</button> */}
-            </div>
-          </Form>
-        </div>
-      )}
-    </Formik>
+    <form
+      name="contact"
+      method="post"
+      action="/thanks/"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}>
+      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label>
+          Don’t fill this out:{" "}
+          <input name="bot-field" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your name:
+          <br />
+          <input type="text" name="name" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your email:
+          <br />
+          <input type="email" name="email" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your company:
+          <br />
+          <input type="text" name="company" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your telephone:
+          <br />
+          <input type="text" name="telephone" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <br />
+          <textarea name="message" onChange={handleChange} />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
-};
+}
 
-export default ContactForm;
+// import React from "react";
+// import { navigate } from "gatsby-link";
+// import { Formik, Form } from "formik";
+// import TextField from "../components/TextField";
+// import MessageField from "../components/MessageField";
+// import * as Yup from "yup";
+
+// function encode(data) {
+//   return Object.keys(data)
+//     .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//     .join("&");
+// }
+
+// const ContactForm = () => {
+//   const [state, setState] = React.useState({});
+
+//   const handleChange = (e) => {
+//     setState({ ...state, [e.target.name]: e.target.value });
+//   };
+
+//   const validate = Yup.object({
+//     name: Yup.string()
+//       .max(30, "Must be 30 characters or less")
+//       .required("Your name is required"),
+//     email: Yup.string()
+//       .email("Email is invalid")
+//       .required("Your email is required"),
+//     company: Yup.string()
+//       .max(30, "Must be 30 characters or less")
+//       .required("Your company name is required"),
+//     telephone: Yup.string()
+//       .max(30, "Must be 30 characters or less")
+//       .required("Your Telephone number is required"),
+//     message: Yup.string()
+//       .max(250, "Must be 250 characters or less")
+//       .required("Your message is required"),
+//   });
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const form = e.target;
+//     fetch("/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: encode({
+//         "form-name": form.getAttribute("name"),
+//         // ...state,
+//       }),
+//     })
+//       .then(() => navigate(form.getAttribute("action")))
+//       .catch((error) => alert(error));
+//   };
+
+//   return (
+//     <Formik
+//       initialValues={{
+//         firstName: "",
+//         lastName: "",
+//         email: "",
+//         message: "",
+//       }}
+//       validationSchema={validate}
+//       // onSubmit={handleSubmit}
+//       onSubmit={console.log(initialValues)}>
+//       {(formik) => (
+//         <div className="form">
+//           <Form
+//             name="contact"
+//             method="post"
+//             data-netlify="true"
+//             data-netlify-honeypot="bot-field">
+//             <input type="hidden" name="form-name" value="contact" />
+//             <p hidden>
+//               <label>
+//                 Don’t fill this out:{" "}
+//                 <input name="bot-field" onChange={handleChange} />
+//               </label>
+//             </p>
+//             <div className="contact-details">
+//               <TextField
+//                 label="Name"
+//                 name="name"
+//                 type="text"
+//                 onChange={handleChange}
+//               />
+//               <TextField
+//                 label="Email"
+//                 name="email"
+//                 type="email"
+//                 onChange={handleChange}
+//               />
+//               <TextField
+//                 label="Company"
+//                 name="company"
+//                 type="text"
+//                 onChange={handleChange}
+//               />
+//               <TextField
+//                 label="Telephone"
+//                 name="telephone"
+//                 type="text"
+//                 onChange={handleChange}
+//               />
+//             </div>
+//             <div className="contact-message">
+//               <MessageField label="Message" name="message" type="textarea" />
+//             </div>
+//             <div className="button-wrap">
+//               <button type="submit">Submit</button>
+//               {/* <button type="reset">Reset</button> */}
+//             </div>
+//           </Form>
+//         </div>
+//       )}
+//     </Formik>
+//   );
+// };
+
+// export default ContactForm;
---
+++ src/pages/thanks.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const thanks = () => {
+  return (
+    <Layout>
+      <div className="thanks-wrap">
+        <iframe
+          title="topfix"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19887.101136202415!2d-2.585676027062345!3d51.46021932187252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f6d3ce2d293f%3A0x7ec9f06531201b88!2sTopfix%20Interiors%20Ltd!5e0!3m2!1sen!2suk!4v1622211935162!5m2!1sen!2suk"
+          width="800"
+          height="600"
+          allowFullScreen=""
+          loading="lazy"></iframe>
+        <div className="thanks-message">
+          <h1>Thanks for contacting us</h1>
+          <h2>We will be back in touch soon!</h2>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default thanks;
