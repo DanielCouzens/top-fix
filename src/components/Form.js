0c079748a8c3fbace5fd9334ee65287d24113384
Daniel Couzens <danecouzens@gmail.com>
2021-07-17 14:11:40 +0100

    formik

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { navigate } from "gatsby-link";
-// import Layout from '../layout'
+import { Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function encode(data) {
   return Object.keys(data)
@@ -30,61 +31,84 @@ export default function Contact() {
       .catch((error) => alert(error));
   };
 
+  const validate = Yup.object({
+    name: Yup.string()
+      .max(30, "Must be 30 characters or less")
+      .required("Your name is required"),
+    email: Yup.string()
+      .email("Email is invalid")
+      .required("Your email is required"),
+    company: Yup.string()
+      .max(30, "Must be 30 characters or less")
+      .required("Your company name is required"),
+    telephone: Yup.string()
+      .max(30, "Must be 30 characters or less")
+      .required("Your Telephone number is required"),
+    message: Yup.string()
+      .max(250, "Must be 250 characters or less")
+      .required("Your message is required"),
+  });
+
   return (
-    <form
-      name="contact"
-      method="post"
-      action="/thanks/"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      onSubmit={handleSubmit}>
-      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-      <input type="hidden" name="form-name" value="contact" />
-      <p hidden>
-        <label>
-          Don’t fill this out:{" "}
-          <input name="bot-field" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your name:
-          <br />
-          <input type="text" name="name" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your email:
-          <br />
-          <input type="email" name="email" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your company:
-          <br />
-          <input type="text" name="company" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your telephone:
-          <br />
-          <input type="text" name="telephone" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message:
-          <br />
-          <textarea name="message" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <Formik method="post" validationSchema={validate}>
+      <form
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        name="contact"
+        onSubmit={handleSubmit}>
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{" "}
+            <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <Field type="text" name="name" onChange={handleChange} />
+            <ErrorMessage component="p" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <Field type="email" name="email" onChange={handleChange} />
+            <ErrorMessage component="p" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your company:
+            <br />
+            <Field type="text" name="company" onChange={handleChange} />
+            <ErrorMessage component="p" name="company" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your telephone:
+            <br />
+            <Field type="text" name="telephone" onChange={handleChange} />
+            <ErrorMessage component="p" name="telephone" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Formik>
   );
 }
 
@@ -94,52 +118,6 @@ export default function Contact() {
 // import TextField from "../components/TextField";
 // import MessageField from "../components/MessageField";
 // import * as Yup from "yup";
-
-// function encode(data) {
-//   return Object.keys(data)
-//     .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&");
-// }
-
-// const ContactForm = () => {
-//   const [state, setState] = React.useState({});
-
-//   const handleChange = (e) => {
-//     setState({ ...state, [e.target.name]: e.target.value });
-//   };
-
-//   const validate = Yup.object({
-//     name: Yup.string()
-//       .max(30, "Must be 30 characters or less")
-//       .required("Your name is required"),
-//     email: Yup.string()
-//       .email("Email is invalid")
-//       .required("Your email is required"),
-//     company: Yup.string()
-//       .max(30, "Must be 30 characters or less")
-//       .required("Your company name is required"),
-//     telephone: Yup.string()
-//       .max(30, "Must be 30 characters or less")
-//       .required("Your Telephone number is required"),
-//     message: Yup.string()
-//       .max(250, "Must be 250 characters or less")
-//       .required("Your message is required"),
-//   });
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const form = e.target;
-//     fetch("/", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//       body: encode({
-//         "form-name": form.getAttribute("name"),
-//         // ...state,
-//       }),
-//     })
-//       .then(() => navigate(form.getAttribute("action")))
-//       .catch((error) => alert(error));
-//   };
 
 //   return (
 //     <Formik
