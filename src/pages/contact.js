a4e3127d40c3e24d5ac33eb4adbe76e9e5f3b7df
Daniel Couzens <danecouzens@gmail.com>
2022-01-18 13:27:09 +0000

    zoomed in close map

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,19 +14,11 @@ const contact = () => {
       <div className="contact-page-wrap">
         <div className="map-wrap">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4971.775648135884!2d-2.572538566349905!3d51.4602159796274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f6d3ce2d293f%3A0x7ec9f06531201b88!2sTopfix%20Interiors%20Ltd!5e0!3m2!1sen!2suk!4v1642440636108!5m2!1sen!2suk"
-            width="800"
-            height="600"
-            // style="border:0;"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2485.887642025284!2d-2.5703552487538515!3d51.46021932187285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f6d3ce2d293f%3A0x7ec9f06531201b88!2sTopfix%20Interiors%20Ltd!5e0!3m2!1sen!2suk!4v1642512372830!5m2!1sen!2suk"
+            width="600"
+            height="450"
             allowfullscreen=""
             loading="lazy"></iframe>
-          {/* <iframe
-            title="topfix"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19887.101136202415!2d-2.585676027062345!3d51.46021932187252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f6d3ce2d293f%3A0x7ec9f06531201b88!2sTopfix%20Interiors%20Ltd!5e0!3m2!1sen!2suk!4v1622211935162!5m2!1sen!2suk"
-            width="800"
-            height="600"
-            allowFullScreen=""
-            loading="lazy"></iframe> */}
           <div className="contact-details">
             <h1>Contact Us</h1>
 
